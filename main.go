1f2d2a306558f4a1fe9087da45284f210547c426
unknown <ibeawuchiclaret@gmail.com>
2024-01-11 21:18:55 -0800

    Multiple return values

--- main.go
+++ main.go
@@ -2,26 +2,52 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"strings"
 )
 
-func sayGreeting(n string) {
-	fmt.Printf("Good morning %v \n", n)
-}
-func sayBye(n string) {
-	fmt.Printf("Bye bye %v \n", n)
-}
-func cycleNames(n []string, f func(string)) {
-	for _, v := range n {
-		f(v)
-	}
-}
+// func sayGreeting(n string) {
+// 	fmt.Printf("Good morning %v \n", n)
+// }
+// func sayBye(n string) {
+// 	fmt.Printf("Bye bye %v \n", n)
+// }
+// func cycleNames(n []string, f func(string)) {
+// 	for _, v := range n {
+// 		f(v)
+// 	}
+// }
 
-func circleArea(r float64) float64 {
-	return math.Pi * r * r
+// func circleArea(r float64) float64 {
+// 	return math.Pi * r * r
+// }
+
+func getInitials(n string) (string, string) {
+	s := strings.ToUpper(n)
+	names := strings.Split(s, " ")
+
+	var initials []string
+	for _, v := range names {
+		initials = append(initials, v[:1])
+	}
+
+	if len(initials) > 1 {
+		return initials[0], initials[1]
+	}
+
+	return initials[0], "_"
 }
 
 func main() {
+
+	fn1, sn1 := getInitials("annie leonhart")
+	fmt.Println(fn1, sn1)
+
+	fn2, sn2 := getInitials("eren jaeger")
+	fmt.Println(fn2, sn2)
+
+	fn3, sn3 := getInitials("titans")
+	fmt.Println(fn3, sn3)
+
 	// // strings
 	// var nameOne string = "claret"
 	// var nameTwo = "martins"
@@ -173,13 +199,13 @@ func main() {
 	// sayGreeting("martins")
 	// sayBye("claret")
 
-	cycleNames([]string{"gloria", "chinwendu", "kaduru"}, sayGreeting)
-	cycleNames([]string{"gloria", "chinwendu", "kaduru"}, sayBye)
+	// cycleNames([]string{"gloria", "chinwendu", "kaduru"}, sayGreeting)
+	// cycleNames([]string{"gloria", "chinwendu", "kaduru"}, sayBye)
 
-	a1 := circleArea(10.5)
-	a2 := circleArea(15)
+	// a1 := circleArea(10.5)
+	// a2 := circleArea(15)
 
-	fmt.Println(a1, a2)
-	fmt.Printf("circle 1 is %0.3f and circle 2 is %0.3f", a1, a2)
+	// fmt.Println(a1, a2)
+	// fmt.Printf("circle 1 is %0.3f and circle 2 is %0.3f", a1, a2)
 
 }
